f08218c7736ea2e5c411abcfbe72ee49d934f753
manuelarrieta1048-create <manuelarrieta1048@gmail.com>
2026-03-13 08:01:44 -0500

    cambio(1)
    ca

--- SISTEMA DE CALCULO DE DESCUENTO POR MENBRESIA.py
+++ SISTEMA DE CALCULO DE DESCUENTO POR MENBRESIA.py
@@ -1,17 +1,18 @@
 # Pedimos al usuario el valor de la compra
-valor_compra = float(input("Ingrese el valor de la compra: "))
+valor_compra = float(input("Ingrese el valor de la compra: "))# float lo que hace es convertir un valor en decimal.
 
 # Pedimos el tipo de cliente (normal o premium)
 tipo_cliente = input("Ingrese el tipo de cliente (normal/premium): ")
 
 # Verificamos si el cliente es premium
-if tipo_cliente.lower() == "premium":
+if tipo_cliente.lower() == "premium":# lower convirte el texo en minuscula.(if,si)
     descuento = valor_compra * 0.15  # Calculamos el 15% de descuento
-else:
-    descuento = 0  # Si no es premium, no hay descuento
+else:# si no se hace esto
+    descuento = 0  # Si no es premium, no hay descuento.
 
 # Calculamos el total a pagar
 total_pagar = valor_compra - descuento
 
 # Mostramos el resultado final
 print("El total a pagar es:", total_pagar)
+#hola
